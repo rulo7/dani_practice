b0a8ddb9b8e8e963fd48ea1b65c548c1755a6bdd
Daniel <danfg95@gmail.com>
2018-10-16 13:19:38 +0200

    Merge branch 'develop' of https://github.com/rulo7/dani_practice into develop
    # Conflicts:
    #	app/src/main/java/com/quohealth/danipractice/MainActivity.kt
    #	app/src/main/res/layout/activity_splash.xml

--- app/src/main/java/com/quohealth/danipractice/presentation/ListUsersActivity.kt
+++ app/src/main/java/com/quohealth/danipractice/presentation/ListUsersActivity.kt
@@ -1,53 +1,137 @@
 package com.quohealth.danipractice.presentation
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import com.quohealth.danipractice.R
+import com.quohealth.danipractice.domain.Worker
+import com.quohealth.danipractice.domain.factory
+import kotlinx.android.synthetic.main.activity_list_users.*
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.android.synthetic.main.users_child.view.*
 
 class ListUsersActivity : AppCompatActivity() {
 
-    lateinit var users_list : RecyclerView
-    var users:MutableList<String> = ArrayList()
+    lateinit var users_list: RecyclerView
+    //var users:MutableList<String> = ArrayList()
+
+    val interactor = factory.interactor
+
+    val progressBar: ProgressBar = this.loadingBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_users)
 
-        users_list =  findViewById(R.id.user_list) as RecyclerView
+        //loadData()
+        users_list = findViewById(R.id.user_list) as RecyclerView
+        users_list.layoutManager = LinearLayoutManager(this)
+
+        progressBar.visibility = View.VISIBLE
+        interactor.retrieveWorkers { workers ->
+            progressBar.visibility = View.GONE
+            users_list.adapter = UserAdapter(workers,this)
+        }
+
 
     }
 
-    class UserAdapter(items : List<String>) : RecyclerView.Adapter<UserAdapter.ViewHolder>(){
-        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
+    //class UserAdapter(items : List<String>, ctx: Context) : RecyclerView.Adapter<UserAdapter.ViewHolder>(){
+    class UserAdapter(items: List<Worker>, ctx: Context) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+        var list = items
+        var context = ctx
 
         override fun getItemCount(): Int {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            return list.size
         }
 
-        override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.users_child, parent, false))
         }
 
-        class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
-            val name = v.user_name
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder?.userName?.text = list.get(position).name
+            holder?.userRole?.text = list.get(position).role
+            //holder?.userImage?.
+        }
+
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+            val userName = v.user_name
+            val userRole = v.user_role
+            val userImage = v.user_image
         }
 
     }
 
-    fun loadData(){
+    /*fun loadData(){
+        users.add("Pedro")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
         users.add("Daniel")
         users.add("Raul")
         users.add("Gustavo")
         users.add("Alvaro")
-
-
-    }
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+        users.add("Daniel")
+        users.add("Raul")
+        users.add("Gustavo")
+        users.add("Alvaro")
+    }*/
 
 
 }
