c65d8b3a0d186f7d277f19b089a4c9d48bec65cc
Daniel <danfg95@gmail.com>
2018-10-18 13:41:09 +0200

    Merge branch 'develop' of https://github.com/rulo7/dani_practice into develop
    # Conflicts:
    #	app/src/main/java/com/quohealth/danipractice/MainActivity.kt
    #	app/src/main/res/layout/activity_splash.xml

---
+++ app/src/main/java/com/quohealth/danipractice/presentation/UserAdapter.kt
@@ -0,0 +1,38 @@
+package com.quohealth.danipractice.presentation
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.quohealth.danipractice.R
+import com.quohealth.danipractice.domain.Worker
+import com.quohealth.danipractice.domain.dp
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.users_child.view.*
+
+class UserAdapter(private val items: List<Worker>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.users_child, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.itemView.apply {
+            val element = items[position]
+            user_name?.text = element.name
+            user_role?.text = element.role
+            Picasso.get()
+                    .load(element.image)
+                    .resize(context.dp(80), context.dp(80))
+                    .into(user_image)
+        }
+    }
+
+    companion object
+    class UserViewHolder(v: View) : RecyclerView.ViewHolder(v)
+}
